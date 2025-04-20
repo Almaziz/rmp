c225d01e1b2bf4f6c25210509ddfc2419c305655
Almaz <izbasarovalmaz1@gmail.com>
2025-04-20 15:28:30 +0300

    rabota

---
+++ MikushinaNE/src/mikushinane/ssss.java
@@ -0,0 +1,12 @@
+package mikushinane;
+
+public class ssss {
+
+	public static void main(String[] args) {
+		int ages = 18;
+		System.out.print("Юлиана ");
+		System.out.println("18");
+		System.out.println("Лена " + ages);
+		
+	}
+}
